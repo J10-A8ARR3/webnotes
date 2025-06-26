7979194e249d119920fb2bad5ffad2ec50061968
Jeo Abarre <136715818+J10-A8ARR3@users.noreply.github.com>
2025-06-26 23:26:15 +0800

    add post func

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Post from './Post';
 import classes from '../styles/PostList.module.css';
@@ -6,27 +6,58 @@ import NewPost from './NewPost';
 import Modal from './Modal';
 
 function PostList ({ isPosting, onStopPosting }) {
-    const [posts, setPosts] = useState([]);
-    
-    function addPostHandler(postData) {
-        setPosts((existingPosts) => [postData, ...existingPosts]);
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
     }
+    fetchPosts();
+  }, []);
 
-    return (
-        <>
-        { isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost 
-            onCancel={onStopPosting}
-            onAddPost={addPostHandler}
-          />
-        </Modal>
-        )}
+  function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts',{
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+      <>
+      { isPosting && (
+      <Modal onClose={onStopPosting}>
+        <NewPost 
+          onCancel={onStopPosting}
+          onAddPost={addPostHandler}
+        />
+      </Modal>
+      )}
+      { !isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post) => <Post author={post.author} body={post.body} /> )}
+          {posts.map((post) => 
+          <Post key={post.body} author={post.author} body={post.body} /> )}
         </ul>
-        </>
-    )
+      )}
+      { !isFetching && posts.length === 0 && (
+        <p className={classes.noPosts}>
+          No posts found. Start posting!
+        </p>
+      )}
+      { isFetching && (
+        <p className={classes.noPosts}>
+          Loading posts...
+        </p>
+      )}
+    </>
+  )
 }
 
 export default PostList;
